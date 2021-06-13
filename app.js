30d49ad5a52dc931eb4b341f1e70f74d534aa998
Alexander Shiryaev <alex_sh_v@mail.ru>
2021-06-13 16:40:41 +0300

    gallery update

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-const slides = document.querySelectorAll('.slide');
+const slidesPlugin = (activeSlide = 0) => {
+  const slides = document.querySelectorAll('.slide');
 
-// for (let slide of slides) {
-//   slide.addEventListener('click', () => {
-//     clearActiveClasses();
-//     slide.classList.add('active');
-//   });
-// }
+  // for (let slide of slides) {
+  //   slide.addEventListener('click', () => {
+  //     clearActiveClasses();
+  //     slide.classList.add('active');
+  //   });
+  // }
 
-slides.forEach((item) => {
-  item.addEventListener('click', () => {
-    clearActiveClasses();
-    item.classList.add('active');
-  });
-});
+  slides[activeSlide].classList.add('active');
 
-const clearActiveClasses = () => {
   slides.forEach((item) => {
-    item.classList.remove('active');
+    item.addEventListener('click', () => {
+      clearActiveClasses();
+      item.classList.add('active');
+    });
   });
+
+  const clearActiveClasses = () => {
+    slides.forEach((item) => {
+      item.classList.remove('active');
+    });
+  };
 };
+
+slidesPlugin(3);
