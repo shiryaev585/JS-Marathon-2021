e12e2d97e5e09804a93387af11143ccc398e73a7
Alexander Shiryaev <alex_sh_v@mail.ru>
2021-06-13 18:30:27 +0300

    hw-2

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
-const slidesPlugin = (activeSlide = 0) => {
-  const slides = document.querySelectorAll('.slide');
+const item = document.querySelector('.item');
+const placeholders = document.querySelectorAll('.placeholder');
 
-  // for (let slide of slides) {
-  //   slide.addEventListener('click', () => {
-  //     clearActiveClasses();
-  //     slide.classList.add('active');
-  //   });
-  // }
+item.addEventListener('dragstart', dragStart);
+item.addEventListener('dragend', dragEnd);
 
-  slides[activeSlide].classList.add('active');
+for (let placeholder of placeholders) {
+  placeholder.addEventListener('dragover', dragOver);
+  placeholder.addEventListener('dragenter', dragEnter);
+  placeholder.addEventListener('dragleave', dragLeave);
+  placeholder.addEventListener('drop', drop);
+}
 
-  slides.forEach((item) => {
-    item.addEventListener('click', () => {
-      clearActiveClasses();
-      item.classList.add('active');
-    });
-  });
+function dragStart(e) {
+  e.target.classList.add('hold');
+  setTimeout(() => {
+    e.target.classList.add('hide');
+  }, 0);
+}
 
-  const clearActiveClasses = () => {
-    slides.forEach((item) => {
-      item.classList.remove('active');
-    });
-  };
-};
+function dragEnd(e) {
+  //   e.target.classList.remove('hold', 'hide');
+  e.target.className = 'item';
+}
 
-slidesPlugin(3);
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.target.classList.add('hovered');
+}
+
+function dragLeave(e) {
+  e.target.classList.remove('hovered');
+}
+
+function drop(e) {
+  e.target.append(item);
+  e.target.classList.remove('hovered');
+}
