8ce4f6c7e34b203038b7e9505b6c482787e67a1f
Alexander Shiryaev <alex_sh_v@mail.ru>
2021-06-14 18:47:39 +0300

    board update

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 const board = document.querySelector('#board');
-const SQUARES_NUMBERS = 450;
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '2ecc71'];
+const SQUARES_NUMBERS = 500;
 
-for(let i = 0; i < SQUARES_NUMBERS; i++) {
-    
+for (let i = 0; i < SQUARES_NUMBERS; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  square.addEventListener('mouseover', () => {
+    setColor(square);
+  });
+
+  square.addEventListener('mouseleave', () => {
+    removeColor(square);
+  });
+
+  board.append(square);
+}
+
+function setColor(elem) {
+  const color = getRandomColor();
+  elem.style.backgroundColor = color;
+  elem.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(elem) {
+  elem.style.backgroundColor = '#1d1d1d';
+  elem.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
 }
