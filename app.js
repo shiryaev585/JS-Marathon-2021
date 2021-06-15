cc24bb38e8a3abd4ba1cd092313470d50b781219
Alexander Shiryaev <alex_sh_v@mail.ru>
2021-06-15 11:12:49 +0300

    board update

--- app.js
+++ app.js
@@ -6,29 +6,25 @@ for (let i = 0; i < SQUARES_NUMBERS; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
 
-  square.addEventListener('mouseover', () => {
-    setColor(square);
-  });
-
-  square.addEventListener('mouseleave', () => {
-    removeColor(square);
-  });
+  square.addEventListener('mouseover', setColor);
+  square.addEventListener('mouseleave', removeColor);
 
   board.append(square);
 }
 
-function setColor(elem) {
+function setColor(e) {
+  const elem = e.target
   const color = getRandomColor();
   elem.style.backgroundColor = color;
   elem.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
-function removeColor(elem) {
+function removeColor(e) {
+  const elem = e.target
   elem.style.backgroundColor = '#1d1d1d';
   elem.style.boxShadow = `0 0 2px #000`;
 }
 
 function getRandomColor() {
-  const index = Math.floor(Math.random() * colors.length);
-  return colors[index];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
